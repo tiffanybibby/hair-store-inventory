a668a399cd016e15edaf8e8bdc914d19b7cfe731
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-08-25 19:30:41 -0400

    resizing elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -92,25 +92,25 @@ function App() {
           <div style={{ margin: "auto",  textAlign:"center" }}>
             <h2>All Wefts</h2>
           </div>
-          <div style={{ margin: "auto", width: "70%" }}>
-          <Row xs={1} md={3} className="g-4" style={{ margin: "10px" }}>
+          <div style={{ margin: "auto", width: "60%"}}>
+          <Row xs={1} md={3} className="g-4" style={{ margin: "0px" }}>
             {Array.from({ length: 1 }).map((_, idx) => (
               <>
                 {wefts.map((weft, index) => (
                   <React.Fragment key={index}>
                     <Col>
-                      <Card style={{ height: "100%"}}>
+                      <Card style={{ height: "100%" , width: "100%" }}>
                         <Card.Img
                           variant="top"
                           src={weft.fields.image}
-                          style={{ marginBottom:"10px", height: "60%", width: "100%" }}
+                          style={{ marginBottom:"10px", height: "160px", width: "100%" }}
                         />
                         <Card.Body>
                           <Card.Title style={{ margin: "20px 0px", width: "100%", height: "40px", textAlign:"center" }} >{weft.fields.name}</Card.Title>
                           <Button
                             variant="primary"
                             type="submit"
-                            style={{ marginTop: "5px", width: "100%" }}
+                            style={{ margin: "5px 0px", width: "100%" }}
                             onClick={() => (window.location.href = `/wefts/${weft.id}`)}
                           >
                             View Details
