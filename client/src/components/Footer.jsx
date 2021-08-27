268d31b4124c123a872369ddb19777f9d30d976f
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-08-27 15:05:05 -0400

    p2 submission

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <Navbar bg="light" variant="light" expand="sm" style={{marginTop:"100px"}}>
-        <Container style={{width:"25%", marginTop:"20px"}}>
+        <Container style={{width:"20%", marginTop:"20px"}}>
             <Nav className="me-auto"> 
               <Nav.Link href="https://www.linkedin.com/in/tiffany-bibby/">©Tiffany D. Bibby 2021</Nav.Link>
             </Nav>
